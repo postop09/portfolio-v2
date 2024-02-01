b6312b1b11fa3ebf7023b393e31060cd9ad73e87
조윤식 <postop@tuneit.io>
2024-02-01 17:49:19 +0900

    #6 <feat: 메인페이지 - 하단 배너 UI 구현>

---
+++ src/components/Outro/Outro.tsx
@@ -0,0 +1,15 @@
+import s from "./Outro.module.css";
+
+const Outro = () => {
+  return (
+    <section className={s.wrapper}>
+      <h2 className={s.title}>
+        읽어줘서
+        <br />
+        압도적 감사
+      </h2>
+    </section>
+  );
+};
+
+export default Outro;
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/Banner/Banner";
 import HardSkillList from "@/components/HardSkillList/HardSkillList";
+import Outro from "@/components/Outro/Outro";
 import ProjectList from "@/components/ProjectList/ProjectList";
 import SoftSkillList from "@/components/SoftSkillList/SoftSkillList";
 import "swiper/css";
@@ -7,12 +8,13 @@ import "swiper/css/pagination";
 
 const index = () => {
   return (
-    <section>
+    <>
       <Banner />
       <ProjectList />
       <HardSkillList />
       <SoftSkillList />
-    </section>
+      <Outro />
+    </>
   );
 };
 
