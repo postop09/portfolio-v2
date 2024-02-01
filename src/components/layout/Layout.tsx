d9b0997453e175e4020e150c58dedc15b25b4931
조윤식 <postop@tuneit.io>
2024-02-01 18:24:07 +0900

    #7 <refactoring: 프로젝트 상세페이지 - 프로젝트 구조 리팩터링>

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,22 +1,13 @@
 import React from "react";
-import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-
-  const isNotOriginPath = (returnValue: any) => {
-    if (router.pathname !== "/") {
-      return returnValue;
-    }
-  };
-
   return (
     <div>
-      {isNotOriginPath(<Header />)}
+      <Header />
       <main style={{ overflow: "hidden" }}>{children}</main>
-      {isNotOriginPath(<Footer />)}
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/project/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h2>PROJECT DETAIL</h2>
+    </div>
+  );
+};
+
+export default index;
