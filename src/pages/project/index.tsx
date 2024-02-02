1a534c5f09718792424e7ab59c0857aa332707df
조윤식 <postop@tuneit.io>
2024-02-02 14:54:25 +0900

    #9 <feat: 프로젝트 상세페이지 - 콘텐츠 컴포넌트 UI 구현>

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,3 +1,4 @@
+import ContentsCard from "@/components/ContentsCard/ContentsCard";
 import ProjectBanner from "@/components/ProjectBanner/ProjectBanner";
 import ProjectLinkList from "@/components/ProjectLinkList/ProjectLinkList";
 import ProjectSpec from "@/components/ProjectSpec/ProjectSpec";
@@ -12,6 +13,19 @@ const index = () => {
       <ProjectSpec />
       <ProjectVideo />
       <ProjectSummary />
+      <ContentsCard />
+      <ContentsCard type="switch" />
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          maxWidth: "1200px",
+          margin: "40px auto",
+        }}
+      >
+        <ContentsCard type="column" />
+        <ContentsCard type="column" />
+      </div>
       <ProjectLinkList />
     </div>
   );
