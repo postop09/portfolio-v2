6d9262ad3ffa7682e7fec0488d137ab6b54863f1
조윤식 <postop@tuneit.io>
2024-02-02 09:59:57 +0900

    #7 <feat: 프로젝트 상세페이지 - 프로젝트 사용 기술 및 스펙 UI 구현>

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,10 +1,12 @@
 import ProjectBanner from "@/components/ProjectBanner/ProjectBanner";
+import ProjectSpec from "@/components/ProjectSpec/ProjectSpec";
 
 const index = () => {
   return (
     <div>
       <h2>PROJECT DETAIL</h2>
       <ProjectBanner />
+      <ProjectSpec />
     </div>
   );
 };
