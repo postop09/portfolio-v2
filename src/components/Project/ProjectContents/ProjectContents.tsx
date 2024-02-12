354f02c19f3b0816e04b43d6f1a43b6f5e0e32db
조윤식 <postop@tuneit.io>
2024-02-12 16:13:38 +0900

    #16 <refactoring: 프로젝트 삭제 - tanStack query 의존성 설치>

--- src/components/Project/ProjectContents/ProjectContents.tsx
+++ src/components/Project/ProjectContents/ProjectContents.tsx
@@ -12,7 +12,6 @@ const ProjectContents = ({ contents }: Props) => {
 
   return objecyKeys.map((key, index) => {
     const row = contents?.[key];
-    // if (row?.length! >= 2) {
     return (
       <div
         key={index}
@@ -36,19 +35,6 @@ const ProjectContents = ({ contents }: Props) => {
         })}
       </div>
     );
-    // } else {
-    //   return row?.map((item, rowIndex) => {
-    //     return (
-    //       <ContentsCard
-    //         key={rowIndex}
-    //         type={item.type}
-    //         src={item.imagePath}
-    //         title={item.title}
-    //         contents={item.contents}
-    //       />
-    //     );
-    //   });
-    // }
   });
 };
 
