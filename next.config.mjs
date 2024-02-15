fdcb41f86da5a655c46ae74a878f227c88a7808a
조윤식 <postop@tuneit.io>
2024-02-15 12:29:10 +0900

    #22 <setting: 배포환경 설정 - nextConfig dirname 설정>

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias["@"] = path.join(__dirname, "src");
+    config.resolve.alias["@"] = path.join("portfolio-v2", "src");
     return config;
   },
   reactStrictMode: true,
